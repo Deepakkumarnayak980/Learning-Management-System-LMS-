17fd8c5ceceb2d2b936ac6745d09089bb0570758
Deepakkumarnayak980 <deepakkumarnayak715@gmail.com>
2024-10-19 09:42:26 +0530

    Configures auto import sorting for eslin

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import { Routers } from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      lms
+     <Routers>
+       { /* <Router path="/" element={<Home />}></Router> */}
+     </Routers>
     </>
   )
 }
