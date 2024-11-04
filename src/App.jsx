934bf4743776cdd7ba2beaafda08235ce0797e58
Deepakkumarnayak980 <deepakkumarnayak715@gmail.com>
2024-11-04 22:34:11 +0530

    Update Project

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,9 @@ import './App.css'
 //import store from '../Redux/Slices/store.js';
 
 import Footer from './components/Footer'
-<<<<<<< HEAD
+
 import Layout from './Layout/HomeLayout'
-=======
->>>>>>> 40792bab96a20c4a7c67f079d6aa0fd4c3eee48f
+
 
 function App() {
 
@@ -17,10 +16,9 @@ function App() {
        { /* <Router path="/" element={<Home />}></Router> */}
      {/*</Routers>*/}
      <Footer />
-<<<<<<< HEAD
+
      <Layout />
-=======
->>>>>>> 40792bab96a20c4a7c67f079d6aa0fd4c3eee48f
+
     </>
   )
 }
