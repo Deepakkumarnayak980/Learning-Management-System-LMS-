6ed11bce34b02b62aef73d8f12b614df9147e363
Deepakkumarnayak980 <deepakkumarnayak715@gmail.com>
2024-11-04 20:56:36 +0530

    New_Changes

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+import './App.css'
+//import store from '../Redux/Slices/store.js';
+
+import Footer from './components/Footer'
+import Layout from './Layout/HomeLayout'
+
+function App() {
+
+
+  return (
+    <>
+     {/*<Routers>*/}
+       { /* <Router path="/" element={<Home />}></Router> */}
+     {/*</Routers>*/}
+     <Footer />
+     <Layout />
+    </>
+  )
+}
+
+export default App
