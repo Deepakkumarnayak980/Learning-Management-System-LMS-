5c95146d5647b6b61a02f8479de9f9c057731a1e
Deepakkumarnayak980 <deepakkumarnayak715@gmail.com>
2024-11-06 22:42:34 +0530

    Added about us page and fixed sidebar login signup button

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -13,7 +13,7 @@ function AboutUs() {
                 <div className="flex items-center gap-5 mx-10">
                     <section className="w-1/2 space-y-10">
                         <h1 className="text-5xl text-yellow-400 font-bold font-serif">
-                            Affordable And quality education
+                            Affordable And Quality Education
                         </h1>
                         <p className="text-xl text-gray-200">
                             Our goal is to provide affordable and quality education to the world.
@@ -23,10 +23,7 @@ function AboutUs() {
 
                     <div className="w-1/2">
                         <img
-                            id="test1"
-                            style={{
-                                filter: "drop-shadow(0px 10px 10px rgb(0,0,0))"  // Fixed typo in RGB
-                            }}
+                            style={{ filter: "drop-shadow(0px 10px 10px rgb(0,0,0))" }}
                             alt="about main image"
                             className="drop-shadow-2xl"
                             src={aboutMainImage}
@@ -35,67 +32,57 @@ function AboutUs() {
                 </div>
 
                 {/* Carousel Controls */}
-                <div className="carousel w-1/2 my-16 m-auto">
+                <div className="carousel w-1/2 m-auto my-16">
+                
                     {/* Slide 1 */}
-                    <div id="slide1" className="carousel-item relative w-full">
-                        <div className="flex flex-col items-center justify-center gap-4 px-[15%]">
-                            <img src={api} className="w-40 rounded-full border-2 border-gray-400" alt="API Image" />
-                            <p className="text-xl text-gray-200">API Abdul Kalam.</p>
-                        </div>
-                        <div className="absolute w-[50%] flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                            <a href="#slide4" className="btn btn-circle">❮</a>
+                    <div id="slide1" className="carousel-item relative w-full flex flex-col items-center justify-center gap-4">
+                        <img src={api} className="w-40 rounded-full border-2 border-gray-400" alt="API Image" />
+                        <h1 className="text-2xl font-semibold">APJ Abdul Kalam</h1>
+                        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                            <a href="#slide5" className="btn btn-circle">❮</a>
                             <a href="#slide2" className="btn btn-circle">❯</a>
                         </div>
                     </div>
 
                     {/* Slide 2 */}
-                    <div id="slide2" className="carousel-item relative w-full">
-                        <div className="flex flex-col items-center justify-center gap-4 px-[15%]">
-                            <img src={billGates} className="w-40 rounded-full border-2 border-gray-400" alt="Bill Gates" />
-                            <p className="text-xl text-gray-200">Bill Gates.</p>
-                        </div>
-                        <div className="absolute w-[50%] flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                    <div id="slide2" className="carousel-item relative w-full flex flex-col items-center justify-center gap-4">
+                        <img src={billGates} className="w-40 rounded-full border-2 border-gray-400" alt="Bill Gates" />
+                        <p className="text-2xl font-semibold">Bill Gates</p>
+                        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                             <a href="#slide1" className="btn btn-circle">❮</a>
                             <a href="#slide3" className="btn btn-circle">❯</a>
                         </div>
                     </div>
 
                     {/* Slide 3 */}
-                    <div id="slide3" className="carousel-item relative w-full">
-                        <div className="flex flex-col items-center justify-center gap-4 px-[15%]">
-                            <img src={einstein} className="w-40 rounded-full border-2 border-gray-400" alt="Einstein" />
-                            <p className="text-xl text-gray-200">Einstein</p>
-                        </div>
-                        <div className="absolute w-[50%] flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                    <div id="slide3" className="carousel-item relative w-full flex flex-col items-center justify-center gap-4">
+                        <img src={einstein} className="w-40 rounded-full border-2 border-gray-400" alt="Einstein" />
+                        <p className="text-2xl font-semibold">Albert Einstein</p>
+                        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                             <a href="#slide2" className="btn btn-circle">❮</a>
                             <a href="#slide4" className="btn btn-circle">❯</a>
                         </div>
                     </div>
 
                     {/* Slide 4 */}
-                    <div id="slide4" className="carousel-item relative w-full">
-                        <div className="flex flex-col items-center justify-center gap-4 px-[15%]">
-                            <img src={nelsonMandela} className="w-40 rounded-full border-2 border-gray-400" alt="Nelson Mandela" />
-                            <p className="text-xl text-gray-200">Nelson Mandela</p>
-                        </div>
-                        <div className="absolute w-[50%] flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                    <div id="slide4" className="carousel-item relative w-full flex flex-col items-center justify-center gap-4">
+                        <img src={nelsonMandela} className="w-40 rounded-full border-2 border-gray-400" alt="Nelson Mandela" />
+                        <p className="text-2xl font-semibold">Nelson Mandela</p>
+                        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                             <a href="#slide3" className="btn btn-circle">❮</a>
                             <a href="#slide5" className="btn btn-circle">❯</a>
                         </div>
                     </div>
 
                     {/* Slide 5 (Steve Jobs) */}
-                    <div id="slide5" className="carousel-item relative w-full">
-                        <div className="flex flex-col items-center justify-center gap-4 px-[15%]">
-                            <img src={steveJobs} className="w-40 rounded-full border-2 border-gray-400" alt="Steve Jobs" />
-                            <p className="text-xl text-gray-200">Steve Jobs</p>
-                        </div>
-                        <div className="absolute w-[50%] flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                    <div id="slide5" className="carousel-item relative w-full flex flex-col items-center justify-center gap-4">
+                        <img src={steveJobs} className="w-40 rounded-full border-2 border-gray-400" alt="Steve Jobs" />
+                        <p className="text-2xl font-semibold">Steve Jobs</p>
+                        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                             <a href="#slide4" className="btn btn-circle">❮</a>
                             <a href="#slide1" className="btn btn-circle">❯</a>
                         </div>
                     </div>
-
                 </div>
 
             </div>
